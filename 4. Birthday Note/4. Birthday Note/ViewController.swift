a96f34e45fe06e525ba6678353e7971614f3c958
Bagusrizky <joshua_saputra@bca.co.id>
2023-09-26 17:15:50 +0700

    add user default

--- 4. Birthday Note/4. Birthday Note/ViewController.swift
+++ 4. Birthday Note/4. Birthday Note/ViewController.swift
@@ -10,19 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var birthdayField: UITextField!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayField: UITextField!
     @IBOutlet weak var birthdayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthDay = UserDefaults.standard.object(forKey: "birthDay")
+        
+        if let myName = storedName as? String{
+            nameLabel.text = myName
+        }
+        
+        if let myBirthday = storedBirthDay as? String{
+            birthdayLabel.text = myBirthday
+        }
+        
     }
 
     @IBAction func saveButton(_ sender: UIButton) {
-        nameLabel.text = nameField.text
-        birthdayLabel.text = birthdayField.text
+        
+        UserDefaults.standard.set(nameField.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayField.text!, forKey: "birthDay")
+        
+        nameLabel.text = "Name : \(nameField.text!)"
+        birthdayLabel.text = "Birthday : \(birthdayField.text!)"
+        
     }
     
-
 }
 
