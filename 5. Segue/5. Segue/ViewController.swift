8c1b5c7d8b0b3c40d4f40aad595dc91d392eeef1
Bagusrizky <joshua_saputra@bca.co.id>
2023-10-02 09:43:50 +0700

    fix perform segue

--- 5. Segue/5. Segue/ViewController.swift
+++ 5. Segue/5. Segue/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userName = ""
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextButton(_ sender: UIButton) {
+        userName = nameTextField.text!
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC" {
+            let desinationVC = segue.destination as! SecondViewController
+            desinationVC.myName = userName
+        }
+           
+    }
 }
 
