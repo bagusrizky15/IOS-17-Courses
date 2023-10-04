7b967d0186e424432940f521a73a53917d759762
Bagusrizky <joshua_saputra@bca.co.id>
2023-10-04 15:02:25 +0700

    add highscore

--- 9. Catch the Kenny/9. Catch the Kenny/ViewController.swift
+++ 9. Catch the Kenny/9. Catch the Kenny/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     var score = 0
     var timer = Timer()
     var counter = 0
+    var frogArray = [UIImageView]()
+    var randomFrogShow = Timer()
+    var highScore = 0
 
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -29,8 +32,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let storedHighScore = UserDefaults.standard.object(forKey: "highScore")
+        if storedHighScore != nil{
+            highScore = storedHighScore as! Int
+        }
+        highScoreLabel.text = "Highscore: \(highScore)"
+        
         counter = 10
-        timeLabel.text = "Timer: \(counter)"
+        timeLabel.text = "Time: \(counter)"
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTime), userInfo: nil, repeats: true)
         scoreLabel.text = "Score: \(score)"
         
@@ -63,15 +72,61 @@ class ViewController: UIViewController {
         frog7.addGestureRecognizer(tap7)
         frog8.addGestureRecognizer(tap8)
         frog9.addGestureRecognizer(tap9)
+        
+        frogArray = [frog1, frog2, frog3, frog4, frog5, frog6, frog7, frog8, frog9]
+        randomFrogShow = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideFrog), userInfo: nil, repeats: true)
+        hideFrog()
+    }
+    
+    @objc func hideFrog(){
+        for frog in frogArray {
+            frog.isHidden = true
+        }
+        
+        let randomFrog = Int(arc4random_uniform(UInt32(frogArray.count-1)))
+        frogArray[randomFrog].isHidden = false
     }
     
     @objc func countTime(){
-        timeLabel.text = "Timer: \(counter)"
+        timeLabel.text = "Time: \(counter)"
         counter -= 1
         
         if counter == 0 {
             timer.invalidate()
+            randomFrogShow.invalidate()
             timeLabel.text = "Time's Up"
+            
+            for frog in frogArray {
+                frog.isHidden = true
+            }
+            
+            let alert = UIAlertController(title: "Time's Up", message: "Wanna play again?", preferredStyle: UIAlertController.Style.alert)
+            
+            let playAgain = UIAlertAction(title: "Yes", style: UIAlertAction.Style.default){
+                (UIAlertAction) in
+                
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text = "Time: \(self.counter)"
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countTime), userInfo: nil, repeats: true)
+                self.randomFrogShow = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideFrog), userInfo: nil, repeats: true)
+                
+                
+            }
+            let cancelButton = UIAlertAction(title: "No", style: UIAlertAction.Style.cancel)
+            
+            alert.addAction(playAgain)
+            alert.addAction(cancelButton)
+            present(alert, animated: true, completion: {
+                if self.score > self.highScore {
+                    self.highScore = self.score
+                }
+                
+                UserDefaults.standard.set(self.highScore, forKey: "highScore")
+                self.highScoreLabel.text = "Highscore: \(self.highScore)"
+            })
         }
     }
 
